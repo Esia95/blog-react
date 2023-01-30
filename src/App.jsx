0059fdd11edd1ad74c404b176eef54354d9e27ee
Estera Nowak <esteranowak95@gmail.com>
2023-01-30 20:32:24 +0100

    created app

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import { PostsList } from "./pages/PostsList.jsx";
+
+function App() {
+  return <PostsList />;
+}
+
+export default App;
---
+++ src/pages/PostsList.jsx
@@ -0,0 +1,3 @@
+export const PostsList = () => {
+  return <div>PostList</div>;
+};
