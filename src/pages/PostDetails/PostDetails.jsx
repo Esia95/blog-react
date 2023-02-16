e93c76e4df6f1230a50c1a1f6a6a7c8e8b0b1a23
Estera Nowak <esteranowak95@gmail.com>
2023-02-16 12:18:09 +0100

    created PostDetails and added navigation

---
+++ src/pages/PostDetails/PostDetails.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Loading } from "../../components";
+import { Card } from "antd";
+import { LikeOutlined } from "@ant-design/icons";
+import IconText from "../PostsList/components/IconText";
+
+const PostDetails = () => {
+  const { postId } = useParams();
+  const [post, setPost] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`http://localhost:3000/posts/${postId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPost(data);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Card title={post?.title}>
+          {post?.body}
+          <div>
+            <IconText
+              icon={LikeOutlined}
+              text={post?.likesCount}
+              key="list-vertical-like-o"
+            />
+          </div>
+        </Card>
+      )}
+    </>
+  );
+};
+
+export default PostDetails;
