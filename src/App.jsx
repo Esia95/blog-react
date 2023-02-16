5fb51fce61a29a720800d514c9628d034ba6b02f
Estera Nowak <esteranowak95@gmail.com>
2023-02-16 12:08:08 +0100

    created route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
-// import "./App.css";
-import { PostsList } from "./pages";
+import { Navigate, Route, Routes } from "react-router-dom";
+import MainLayout from "./components/MainLayout";
+import { PostDetails } from "./pages";
+import PostsList from "./pages/PostsList/PostsList";
 
-const App = () => <PostsList />;
+const App = () => (
+  <Routes>
+    <Route element={<MainLayout />}>
+      <Route path="posts">
+        <Route index element={<PostsList />} />
+        <Route path=":postId" element={<PostDetails />} />
+      </Route>
+    </Route>
+    <Route path="*" element={<Navigate to="posts" replace />} />
+  </Routes>
+);
 
 export default App;
