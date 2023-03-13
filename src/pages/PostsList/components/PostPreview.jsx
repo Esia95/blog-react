2d9e41224c61d91c33e04e2d28f395e1eec523c1
Estera Nowak <esteranowak95@gmail.com>
2023-03-13 14:33:08 +0100

    changed PostPreview

--- src/pages/PostsList/components/PostPreview.jsx
+++ src/pages/PostsList/components/PostPreview.jsx
@@ -1,85 +1,44 @@
+import { LikeOutlined } from "@ant-design/icons";
+import { Button, List, Typography } from "antd";
 import { IconText } from "components";
-import { List, Typography, Button, Space, Modal } from "antd";
-import {
-  LikeOutlined,
-  DeleteOutlined,
-  ExclamationCircleOutlined,
-} from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
-import postService from "services/post";
-import { useMutation, useQueryClient } from "react-query";
-import { showSuccessNotification, showErrorNotification } from "helpers";
+import styled from "styled-components";
 
 const { Paragraph } = Typography;
 
+const ExtraContentWrapper = styled.div`
+  display: flex;
+  align-items: end;
+  height: 100%;
+`;
+
 const PostPreview = ({ post }) => {
   const navigate = useNavigate();
-  const queryClient = useQueryClient();
 
   const handleNavigate = (postId) => navigate(`/posts/${postId}`);
 
-  const { isLoading, mutate } = useMutation(
-    () => postService.deletePost(post.id),
-    {
-      onSuccess: () => {
-        showSuccessNotification();
-        queryClient.invalidateQueries("posts");
-      },
-      onError: (error) => {
-        showErrorNotification(error.message);
-      },
-    }
-  );
-
-  const handleOk = () => {
-    mutate();
-  };
-
-  const confirm = () => {
-    Modal.confirm({
-      title: "Delete post",
-      icon: <ExclamationCircleOutlined />,
-      content: "Are you sure you want to delete this post?",
-      onOk: handleOk,
-    });
-  };
-
   return (
     <List.Item
-      key={post.id}
+      key={post.title}
       actions={[
         <IconText
           icon={LikeOutlined}
           text={post.likesCount}
           key="list-vertical-like-o"
         />,
-        <Space>
-          <Button onClick={() => handleNavigate(post.id)}>Read more</Button>
-          <Button
-            type="primary"
-            danger
-            icon={<DeleteOutlined />}
-            onClick={confirm}
-            loading={isLoading}
-          >
-            Delete
-          </Button>
-        </Space>,
       ]}
+      extra={
+        <ExtraContentWrapper>
+          <Button type="primary" onClick={() => handleNavigate(post.id)}>
+            Read more
+          </Button>
+        </ExtraContentWrapper>
+      }
     >
-      <List.Item.Meta
-        title={post.title}
-        description={
-          <Paragraph
-            ellipsis={{
-              rows: 2,
-              expandable: false,
-            }}
-          >
-            {post.body}
-          </Paragraph>
-        }
-      />
+      <List.Item.Meta title={post.title} />
+      <Paragraph ellipsis={{ rows: 2, expandable: false }}>
+        {post.body}
+      </Paragraph>
     </List.Item>
   );
 };
